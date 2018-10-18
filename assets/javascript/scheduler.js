ed7c7d8c2bb9a1ca3ec680d8959e08006fe58997
Angela <acsmoot14@gmail.com>
2018-10-17 23:48:33 -0500

    Attempt to fix the nextTrain time in getTrainArrival method

--- assets/javascript/scheduler.js
+++ assets/javascript/scheduler.js
@@ -21,27 +21,17 @@ firebase.initializeApp(config);
 //////////////////////////////////////////
 var database = firebase.database();
 
-function getTrainArrival(minuteFrequency, firstTimeConverted) {
-  var today = moment();
-  var frequency = 0;
-  var firstTrainTime = 0;
+function getTrainArrival(minutesTilTrain, firstTime) {
+    // var diffTime = today.diff(moment(firstTimeConverted), "minutes");
+  // console.log("diffTime" + diffTime.format()); 
 
-  var diffTime =today.diff(moment(firstTimeConverted), "minutes");
-  console.log("diffTime" + diffTime.format());
-
-  var tRemainder = diffTime % minuteFrequency;
-  console.log("tRemainder" + tRemainder);
-
-  var minutesTilTrain = minuteFrequency - tRemainder;
-  console.log("minutesTilTrain" + minutesTilTrain);
-
-  var nextTrain = today.add(minutesTilTrain, "minutes");
-console.log("the next train"+ nextTrain);
+  var nextTrain = firstTime.add(minutesTilTrain, "MM/dd/YYYY HH:mm");
+  console.log("the next train" + nextTrain);
 
   return nextTrain;
 }
 //capture input
-$("#enterInput").on("click", function(event) {
+$("#enterInput").on("click", function (event) {
   console.log("Get User input");
   event.preventDefault();
 
@@ -78,19 +68,19 @@ $("#enterInput").on("click", function(event) {
   });
 });
 
-database.ref("/trainInfo").on("child_added", function(snapshot) {
+database.ref("/trainInfo").on("child_added", function (snapshot) {
   console.log(snapshot.val());
 
   //dynamically create rows
   var newRow = $("<div>").addClass("row");
   var hardLine = $("<hr>").addClass("my-2");
-
+  currentTime = moment();
   //add input values and calculations
   var trainNameCol = $("<div>")
     .addClass("col-2")
     .text(snapshot.val().trainName);
   console.log(trainNameCol);
-  
+
   var destinationCol = $("<div>")
     .addClass("col-2")
     .text(snapshot.val().destination);
@@ -98,40 +88,48 @@ database.ref("/trainInfo").on("child_added", function(snapshot) {
   var minuteFrequencyCol = $("<div>")
     .addClass("col-2")
     .text(snapshot.val().minuteFrequency);
+  dminuteFrequency = snapshot.val().minuteFrequency;
+  console.log("minuteFrequency" + dminuteFrequency);
 
-    //   var minuteFrequency = moment(snapshot.val().minuteFrequency);
-//   console.log("minuteFrequency" + minuteFrequency.format());
-//   console.log(moment().diff(minuteFrequency, "minutes"));
+  var convertedminuteFrequency = moment(dminuteFrequency, "HH:mm");
+  console.log("dminuteFrequency" + dminuteFrequency);
+
+  //   console.log(moment().diff(minuteFrequency, "minutes"));
 
   var firstTrainTimeCol = $("<div>")
     .addClass("col-2")
     .text(snapshot.val().firstTrainTime);
-  var firstTimeConverted = moment(snapshot.val().firstTrainTime, "HH:mm");
+
+  var firstTime = snapshot.val().firstTrainTime;
+  console.log(firstTime);
+  var firstTimeConverted = moment(firstTime, "HH:mm");
   console.log(" firstTrainTime" + firstTimeConverted.format());
 
-  var diffTime = moment().diff(moment(snapshot.val().firstTrainTime), "minutes");
-   console.log("diffTime" + diffTime.format());
+  var diffTime = firstTimeConverted.diff(currentTime, "minutes");
+  console.log("diffTime" + diffTime);
 
-  // var tRemainder = diffTime % minuteFrequency;
-  // console.log("tRemainder" + tRemainder);
+  var tRemainder = diffTime % dminuteFrequency;
+  console.log("tRemainder" + tRemainder);
 
-  // var minutesTilTrain = minuteFrequency - tRemainder;
-  // console.log("minutesTilTrain" + minutesTilTrain);
+  // Minutes Away  id="minutesAway" (calulated)-->
+  var minutesTilTrain = dminuteFrequency - tRemainder;
+  console.log("minutesTilTrain" + minutesTilTrain);
+
   var minutesAwayCol = $("<div>")
     .addClass("col-2")
-    .text("minutesTilTrain");
+    .text(minutesTilTrain);
 
-//   var nextTrain = getTrainArrival(minuteFrequency, firstTimeConverted);
+  // Next Arrival id="nextArrival" (calulated)
+  var nextTrain = getTrainArrival(minutesTilTrain, firstTime);
   var nextTrainCol = $("<div>")
     .addClass("col-2")
-    .text("nextTrain");
+    .text(nextTrain);
 
   //attach info to DOM
   newRow.append(
     trainNameCol,
     destinationCol,
     minuteFrequencyCol,
-    // firstTrainTimeCol,
     nextTrainCol,
     minutesAwayCol
   );
@@ -139,4 +137,3 @@ database.ref("/trainInfo").on("child_added", function(snapshot) {
     .append(newRow)
     .append(hardLine);
 });
-//make time calculations
